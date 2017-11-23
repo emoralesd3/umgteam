8535229524ffd4fe0b99a64a256de426f3c82d4d
Elvin Morales <elvinmorales48@gmail.com>
2017-11-23 08:55:15 -0600

    corrigiendo errores

--- g-callback.php
+++ g-callback.php
@@ -7,9 +7,11 @@
         $google_client->setAccessToken($_SESSION['access_token']);
     }
     else if(isset($_GET['code'])){
-        $token = $google_client->fetchAccessTokenWithAuthCode($_GET['code']);
-        var_dump($token);
+        $google_client->authenticate($_GET['code']);
+        //$token = $google_client->fetchAccessTokenWithAuthCode($_GET['code']);
+        $token = $google_client->getAccessToken();
         $_SESSION['access_token'] = $token;
+        $google_client->setAccessToken($token);
     }else{
         header('Location: login.php');
         exit();
